47d4de949568c9e079a87d483fac67c9f3fbb239
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-04 13:40:38 -0700

    Create getNotes Query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,1 +1,14 @@
-module.exports = {};
+const { ApolloError, AuthenticationError } = require('apollo-server');
+const Note = require('../models/Note');
+
+exports.getNotes = (_, __, { user }) => {
+  if (!user) throw new AuthenticationError('Must authenticate');
+
+  try {
+    const notes = Note.find({ _id: { $in: user.notes } });
+    if (!notes) return [];
+    return notes;
+  } catch (error) {
+    throw new ApolloError('Some Error');
+  }
+};
