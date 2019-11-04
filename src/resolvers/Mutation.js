c5c4b3c00f3cefed482a134b35c438f3dc82bc33
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-03 20:16:13 -0700

    Create register Mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,1 +1,22 @@
-module.exports = {};
+// const User = require('../models/User');
+const { ApolloError } = require('apollo-server');
+const { validateInput } = require('../utils');
+const { registerSchema } = require('../yupSchemas');
+const User = require('../models/User');
+
+
+exports.register = async (_, args) => {
+  await validateInput(args, registerSchema);
+  const { username, password } = args;
+
+  const user = new User({ username });
+  user.setPassword(password);
+
+  try {
+    await user.save();
+  } catch (error) {
+    throw new ApolloError('Some Error');
+  }
+
+  return user;
+};
