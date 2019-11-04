a0cbfbaa4b2e3998a79e4cec401bfef677022e4c
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-03 21:49:17 -0700

    Add context to apollo

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const db = require('./config/db');
+const { SECRET } = require('./config');
+const { getUser } = require('./utils');
+
+const context = async ({ req }) => {
+  const token = req.headers.authorization || '';
+
+  const user = await getUser(token);
+
+  return {
+    SECRET,
+    user,
+  };
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context,
 });
 
 db.once('open', () => {
