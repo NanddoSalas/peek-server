3ac274ed09d7031c73d8db7bc626cd07a21e981a
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-06 13:08:46 -0700

    Create noteAdded and noteDeleted Subscriptions

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,1 +1,18 @@
-module.exports = {};
+/* eslint-disable eqeqeq */
+const { withFilter } = require('apollo-server');
+const { NOTE__ADDED, NOTE__DELETED } = require('../eventLabels');
+
+
+exports.noteAdded = {
+  subscribe: withFilter(
+    (_, __, { pubsub }) => pubsub.asyncIterator([NOTE__ADDED]),
+    ({ noteAdded: note }, _, { user }) => note.createdBy == user.id,
+  ),
+};
+
+exports.noteDeleted = {
+  subscribe: withFilter(
+    (_, __, { pubsub }) => pubsub.asyncIterator([NOTE__DELETED]),
+    ({ noteDeleted: note }, _, { user }) => note.createdBy == user.id,
+  ),
+};
