026729b891166dd1a9828a027b4ffd553ecaae11
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-08 15:43:37 -0700

    Update .env.example

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,9 @@
 const {
-  DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME,
+  DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME, NODE_ENV,
 } = process.env;
 
 module.exports = {
   mongoUri: `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   SECRET: 'ldeVaDcqH33N6WcQ',
+  NODE_ENV,
 };
