33435093c193793633b921ec06df838fd7ac9191
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-08 15:21:12 -0700

    Create authMiddleware

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,7 @@
-const { sign } = require('jsonwebtoken');
+/* eslint-disable no-empty */
+const { sign, verify } = require('jsonwebtoken');
 const { SECRET } = require('./config');
+const User = require('./models/User');
 
 function createTokens(user) {
   const accesToken = sign(
@@ -24,7 +26,54 @@ function setTokens(res, user) {
   res.cookie('refreshToken', refreshToken);
 }
 
+async function authMiddleware(req, res, next) {
+  const { accesToken, refreshToken } = req.cookies;
+
+  // No tokens found
+  if (!accesToken && !refreshToken) return next();
+
+  try {
+    const { id } = verify(accesToken, SECRET);
+    const user = await User.findById(id);
+    req.user = user;
+    // Valid token
+    return next();
+  } catch (error) {}
+
+  if (!refreshToken) {
+    // No refreshToken found
+    return next();
+  }
+
+  let data;
+
+  try {
+    data = verify(refreshToken, SECRET);
+  } catch (error) {
+    // Invalid refreshToken
+    return next();
+  }
+
+  const user = await User.findById(data.id);
+
+  if (!user) {
+    // No user found
+    return next();
+  }
+
+  if (user.count !== data.count) {
+    // Expired refreshToken
+    return next();
+  }
+
+  setTokens(res, user);
+  req.user = user;
+
+  return next();
+}
+
 module.exports = {
   createTokens,
   setTokens,
+  authMiddleware,
 };
