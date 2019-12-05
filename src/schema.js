9bf63c7d8580820d08fb15ef3bab70216c684b83
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-12-04 21:49:27 -0700

    Add googleAuth mutation to schema

--- src/schema.js
+++ src/schema.js
@@ -1,39 +1,40 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-type Query {
-  getNotes: [Note!]!
-  me: User
-}
+  type Query {
+    getNotes: [Note!]!
+    me: User
+  }
 
-type Mutation {
-  register(username: String!, password: String!, password2: String!): Boolean!
-  login(username: String!, password: String!): User!
-  logout: Boolean!
-  createNote(title: String, text: String): Note!
-  deleteNote(id: ID!): Note!
-  updateNote(id: ID!, title: String!, text: String!): Note!
-}
+  type Mutation {
+    register(username: String!, password: String!, password2: String!): Boolean!
+    login(username: String!, password: String!): User!
+    logout: Boolean!
+    createNote(title: String, text: String): Note!
+    deleteNote(id: ID!): Note!
+    updateNote(id: ID!, title: String!, text: String!): Note!
+    googleAuth(token: String!): User!
+  }
 
-type Subscription {
-  noteAdded: Note!
-  noteDeleted: Note!
-  noteUpdated: Note!
-}
+  type Subscription {
+    noteAdded: Note!
+    noteDeleted: Note!
+    noteUpdated: Note!
+  }
 
-type User {
-  id: ID!
-  username: String!
-  notes: [Note!]!
-}
+  type User {
+    id: ID!
+    name: String!
+    notes: [Note!]!
+  }
 
-type Note {
-  id: ID!
-  title: String
-  text: String
-  createdAt: String!
-  createdBy: User!
-}
+  type Note {
+    id: ID!
+    title: String
+    text: String
+    createdAt: String!
+    createdBy: User!
+  }
 `;
 
 module.exports = typeDefs;
